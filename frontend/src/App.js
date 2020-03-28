88e8521abb78ee89ff4b2526aeb7e7f58a833133
bruno <bruno@f1solucoes.com.br>
2020-03-28 18:58:27 -0300

    Foi implementado um contador simples para entender os conceitos básicos do React.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from './Header'
 
 /**
- * React
- * Componente: É uma função que retorna HTML.
- * JSX (Javascript XML): É quando o HTML está integrado dentro do javascript.
+ * React - conceitos
+ * 
+ * Componente: é uma função que retorna HTML.
+ * JSX (Javascript XML): é quando o HTML está integrado dentro do javascript.
+ * Propriedades: possuem a mesma sintaxe dos atributos HTML (ex.: id="title"), no entanto, são passadas para os componentes ao invés de elementos do HTML.
+ * Estado: toda vez que o componente precisar armazenar uma informação dentro dele, deve ser criado um estado, para que seja possível atualizar essa informação e para que ela seja refletida na interface.
+ * Imutabilidade: dentro do React, por uma questão de performance, a variável de estado nunca deve ser manipulada diretamente. É necessário sobrepor o valor da variável de estado utilizando o método useState().
  */
 
 function App() {
+  // useState() retorna um array com duas posições: Array [valor, função_de_atualização]
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello OmniStack</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
