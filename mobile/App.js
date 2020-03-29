fec79c73d9a767dc8acb932d24106c9df4ad64f4
bruno <bruno@f1solucoes.com.br>
2020-03-29 16:10:06 -0300

    Adicionados os comentários com as diferenças entre ReactJS e React Native.

--- mobile/App.js
+++ mobile/App.js
@@ -4,10 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello OmniStack</Text>
+      <Text style={styles.title}>Hello OmniStack</Text>
     </View>
   );
 }
+
+/**
+ * Diferenças do React Native para o ReactJS
+ * 
+ * Elementos do HTML: não existe semântica no React Native, por isso, não se utiliza as tags convencionais do HTML.
+ * Utiliza-se a tag <View> para containers e <Text> para qualquer tipo de texto.
+ * 
+ * Estilização: ocorre através da propriedade "style" que receberá um objeto contendo a estilização.
+ * Exemplo: <Text style={styles.title}>
+ * 
+ * Flexbox: Por padrão, no React Native, todos os elementos são display flex. Não existem outras formas de display.
+ * 
+ * Propriedades CSS: São separadas por CamelCase.
+ * Exemplo: { alignItems: 'center' }
+ * 
+ * Heranças de estilo: Não existem heranças de estilo no React Native. O estilo deve ser aplicado diretamente no elemento.
+ */
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +33,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#FFF',
+    fontSize: 20,
+  }
 });
