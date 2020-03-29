e9c93748f56a2264d4e53ab6850164f6a3e0ed20
bruno <bruno@f1solucoes.com.br>
2020-03-29 01:58:09 -0300

    Dinamizada a tela de login.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,15 +10,45 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+    // Cria um estado para armazenar o campo ID
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    // Função para validar se a ONG existe
+    async function handleLogin(e) {
+        // Para desabilitar o envio do form e o refresh da página
+        e.preventDefault();
+
+        try {
+            // Faz um POST para a rota de login, utilizando o módulo axios
+            const response = await api.post('/sessions', { id });
+
+            // Salva na sessão do navegador
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            // Direciona o usuário para a tela que contém a listagem dos casos
+            history.push('/profile');
+        } catch (error) {
+           alert('Falha no login, tente novamente.');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
 
-                <form action="">
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input type="text" placeholder="Sua ID" />
+                    <input type="text" 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+
                     <button type="submit" className="button">Entrar</button>
 
                     <Link to="/register" className="back-link">
