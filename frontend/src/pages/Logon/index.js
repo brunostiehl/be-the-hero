edb447172e7051f3fdae62d3cef9b3650a04998b
bruno <bruno@f1solucoes.com.br>
2020-03-28 21:27:53 -0300

    Foi criada a rota e estilizada a tela Register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -18,10 +19,10 @@ export default function Logon() {
                     <input type="text" placeholder="Sua ID" />
                     <button type="submit" className="button">Entrar</button>
 
-                    <a href="/register">
+                    <Link to="/register" className="back-link">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             
